8ed4f158d94614979c27737285d34f1459b52706
kersh <vladimir@shafikov.com>
2017-02-11 12:01:26 +0000

    Add sass:build command

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,18 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('sass:watch', ['sass'], function () {
   gulp.watch('./src/**/*.scss', ['sass']);
+});
+
+gulp.task('sass:build', function () {
+  return gulp.src('./src/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('serve', ['sass:watch']);
 
-gulp.task('build', ['sass']);
+gulp.task('build', ['sass:build']);
 
 gulp.task('default', ['serve']);
