bea79c9a826c6dc116f13d2679b73436d0577e87
kersh <vladimir@shafikov.com>
2017-02-11 11:46:01 +0000

    Add gulp tasks to run sass and sass:watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-    return gulp.src('./src/sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+  return gulp.src('./src/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch('./src/**/*.scss', ['sass']);
 });
+
+gulp.task('serve', ['sass:watch']);
+
+gulp.task('build', ['sass']);
+
+gulp.task('default', ['serve']);
